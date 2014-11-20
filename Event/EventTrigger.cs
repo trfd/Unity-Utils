ae269d39e054f644f009042678f6457228bd6915
unknown <romain.vivier@STUD-VDOC-051.formation.enjmin.fr>
2014-11-20 11:03:59 +0100

    première class

---
+++ Event/EventTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTrigger : MonoBehaviour {
+
+    public string _eventName;
+
+    public void Trigger()
+    {
+        if(_eventName == "" ||_eventName == null)
+        {
+            Debug.LogError("EVent name can't be null, an error must have occured!");
+            return;
+        }
+
+        EventManager.Instance.PlayEvent(_eventName);
+    }
+}
