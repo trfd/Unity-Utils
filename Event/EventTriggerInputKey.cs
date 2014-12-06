103399a726f3317c3f0326a6ebc78b81852f4a5a
trfd <baptiste.dupy@gmail.com>
2014-12-06 21:57:28 +0100

    EventTriggerInputKey and GPActionAnimatorParameter

---
+++ Event/EventTriggerInputKey.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Utils.Event
+{
+	[AddComponentMenu("EventTrigger/Input/EventTriggerInputKey")]
+	public class EventTriggerInputKey : MonoBehaviour
+	{
+		#region Public Members
+
+		public KeyCode _key;
+
+		public GPEventID _eventID;
+
+		#endregion
+
+		#region MonoBehaviour Override
+
+		void Update () 
+		{
+			if(Input.GetKeyDown(_key))
+				EventManager.Instance.PostEvent(_eventID.Name);
+		}
+
+		#endregion
+	}
+}
