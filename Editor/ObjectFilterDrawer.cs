6f64f3793babedbab305a9a2ca19bee0fe5324fc
unknown <baptiste.dupy@STUD-VDOC-057.formation.enjmin.fr>
2015-02-10 19:09:53 +0100

    ObjectFilter

---
+++ Editor/ObjectFilterDrawer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+using Utils;
+
+public class ObjectFilterDrawer : PropertyDrawer
+{
+    
+    
+    public static void Display(string label, ObjectFilter filter)
+    {
+        EditorGUILayout.LabelField(label);
+
+        EditorGUI.indentLevel++;
+
+        EditorGUILayout.LabelField(label);
+        
+        filter._filterMethod = (ObjectFilter.Method)EditorGUILayout.EnumPopup("Method", filter._filterMethod);
+
+       
+        switch(filter._filterMethod)
+        {
+            case ObjectFilter.Method.OBJECTS: DisplayFilterObjects(filter);
+                break;
+            case ObjectFilter.Method.TAGS: DisplayFilterTags(filter);
+                break;
+        }
+
+        EditorGUI.indentLevel--;
+    }
+
+    private static void DisplayFilterObjects(ObjectFilter filter)
+    {
+        for(int i= 0 ; i<filter._objects.Count ; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            filter._objects[i] = (GameObject) EditorGUILayout.ObjectField(filter._objects[i], typeof(GameObject), true);
+
+            if(GUILayout.Button("Remove"))
+            {
+                filter._objects.RemoveAt(i);
+                --i;
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if(GUILayout.Button("Add Object"))
+            filter._objects.Add(null);
+    }
+
+    private static void DisplayFilterTags(ObjectFilter filter)
+    {
+        for(int i=0 ; i<filter._tags.Count ; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            filter._tags[i] = EditorGUILayout.TagField(filter._tags[i]);
+
+            if (GUILayout.Button("Remove"))
+            {
+                filter._tags.RemoveAt(i);
+                --i;
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button("Add Tag"))
+            filter._tags.Add(null);
+    }
+}
---
+++ ObjectFilter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    [System.Serializable]
+    public class ObjectFilter
+    {
+        public enum Method
+        {
+            NONE,
+            ALL,
+            OBJECTS,
+            TAGS
+        }
+
+        #region Private Members
+
+        public Method _filterMethod;
+        public List<GameObject> _objects;
+        public List<string> _tags;
+
+        #endregion
+
+        public bool IsValid(GameObject obj)
+        {
+            switch(_filterMethod)
+            {
+                case Method.ALL: return true;
+                case Method.OBJECTS: return _objects.Contains(obj);
+                case Method.TAGS: return _tags.Contains(obj.tag);
+                default: return false;
+            }
+        }
+    }
+}
+
